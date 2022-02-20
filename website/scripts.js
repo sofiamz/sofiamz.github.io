388e45dc02dd6f05ed154039aefee8703edea7eb
sofiamz <86375841+sofiamz@users.noreply.github.com>
2022-02-20 11:22:15 -0500

    hide/show button

---
+++ website/scripts.js
@@ -0,0 +1,70 @@
+window.onload = function (){
+//console.log(document.getElementsByClassName("logo"));
+//console.log(document.getElementsByClassName("text"));
+//console.log(document.getElementById("button"));
+//console.log(document.getElementsByTagName("li"));
+	
+	var contentDiv= document.getElementById("content");
+	var landingImg= document.getElementsByClassName("headerImage");
+	var newContent = document.createElement("h1");
+	newContent.innerHTML= "this is content";
+	contentDiv.appendChild(newContent);
+
+	
+	console.log(newContent)
+	
+	function turnTextBlue(){
+	newContent.style.color = "140,216,230";
+	contentDiv.style.color= "royalblue";
+
+		
+	}
+	
+	setTimeout(turnTextBlue, 1500);
+	
+	function changeBackground (){
+		contentDiv.style.backgroundColor= "floralwhite";
+	}
+	
+	setTimeout(changeBackground, 1800);
+	
+	
+	//jQuery(document).ready(function(){
+	//console.log(jQuery("li"));	
+	//console.log(jQuery("content"));
+	//	$("#button").click( function(){
+	//	$("#content").hide();
+		
+	//});
+	//$(document).mouseup(function(e){
+	//	var container = $("#button");
+		
+	//	if (!container.is(e.target)
+	//		&& container.has(e.target).lenght===0)
+	//	    {
+	//			$("#content").show();
+	//		}
+	//});
+	//});
+	
+	
+		
+	jQuery(document).ready(function(){
+	console.log(jQuery("li"));	
+	console.log(jQuery("content"));
+	$("#button").on('click', function(e){
+	$("#content").toggle();
+	});
+	//var hiding = $("#button").on('click', function(e){
+	//$("#content").hide();
+	//});
+		
+	//	if (hiding.is(function(e.target))){
+	//		$("#button").on('click', function(){
+	//		$("#content").show();
+	//	})
+			
+	//		}
+	});
+	
+};// JavaScript Document
